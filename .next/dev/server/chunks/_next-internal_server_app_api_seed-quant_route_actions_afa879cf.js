ef70842d3344bb55ccf5f19b84dbb069807ed0e4
Vishal Singh <2214124vishalsingh.2022cse@gmail.com>
2026-02-25 15:25:37 +0530

    chore: remove .next from tracking

--- .next/dev/server/chunks/_next-internal_server_app_api_seed-quant_route_actions_afa879cf.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/seed-quant/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_seed-quant_route_actions_afa879cf.js.map
